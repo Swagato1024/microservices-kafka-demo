38cdee44012442a18f3e7c44b9cfcb6abb5dc8d2
Swagato Chakraborty <swagatochakraborty@Swagatos-MacBook-Pro.local>
2024-06-27 15:11:50 +0530

    Add email-sender and sms-sender consumer group

--- email-service/src/main/java/com/xyz/email_service/kafka/KafkaConsumerService.java
+++ email-service/src/main/java/com/xyz/email_service/kafka/KafkaConsumerService.java
@@ -1,5 +1,23 @@
 package com.xyz.email_service.kafka;
 
-public class OrderConsumer {
-    @
+import com.xyz.domain.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumerService.class);
+
+    @KafkaListener(topics = "order_topic", groupId = "email-sender", containerFactory = "kafkaListenerContainerFactory")
+    public void listen1(OrderEvent event) {
+        logger.info("Received event in email sender group {}", event);
+    }
+
+
+    @KafkaListener(topics = "order_topic",  groupId = "sms-sender", containerFactory = "kafkaListenerContainerFactory")
+    public void listen2(OrderEvent event) {
+        logger.info("Received event in sms sender group {}", event);
+    }
 }
